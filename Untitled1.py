5619e2af4a52ccd68d24d16e595a57b58efef9f8
OranusKsw <112540736+OranusKsw@users.noreply.github.com>
2023-07-14 09:34:03 +0700

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import cv2
+import numpy as np
+#import imutils
+import pandas as pd
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+ 
+# Create a VideoCapture object and read from input file
+# If the input is the camera, pass 0 instead of the video file name
+cap = cv2.VideoCapture(0)
+# Check if camera opened successfully
+if (cap.isOpened()== False): 
+  print("Error opening video stream or file")
+i = 0
+# Read until video is completed
+while cap.isOpened():
+  # Capture frame-by-frame
+  ret, frame = cap.read()
+  if frame is None:
+    print('frame is none')
+    break
+    # Saving the frames with certain namec
+  if ret == True:
+    #laplacian_var = cv2.Laplacian(frame, cv2.CV_64F).var()
+    box, label, count = cv.detect_common_objects(frame)
+    output = draw_bbox(frame, box, label, count)
+    #output = cv2.cvtColor(output,cv2.COLOR_BGR2RGB)
+    print(label)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+      break
+
+ 
+  # Break the loop
+  else: 
+    break
+  cv2.imshow("test", output)
+ 
+# When everything done, release the video capture object
+cap.release()
+ 
+# Closes all the frames
+cv2.destroyAllWindows()
+
